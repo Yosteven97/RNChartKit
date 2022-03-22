9bb41cd4df11a81fcf487eb6cf314ebb9cc14815
Stefanus Yosteven <johansarap1234@gmail.com>
2022-03-23 00:02:18 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {Dimensions, View, Text} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
+
+const screenWidth = Dimensions.get('window').width;
+
+const App = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+        strokeWidth: 2, // optional
+      },
+    ],
+    legend: ['Sunny Days'], // optional
+  };
+
+  const chartConfig = {
+    backgroundColor: '#e26a00',
+    backgroundGradientFrom: '#fb8c00',
+    backgroundGradientTo: '#ffa726',
+    decimalPlaces: 2, // optional, defaults to 2dp
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16,
+    },
+    propsForDots: {
+      r: '6',
+      strokeWidth: '2',
+      stroke: '#ffa726',
+    },
+  };
+
+  return (
+    <View>
+      <Text>Bezier Line Chart</Text>
+      <LineChart
+        data={data}
+        width={screenWidth} // from react-native
+        height={220}
+        // yAxisLabel="Rp"
+        // yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={chartConfig}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </View>
+  );
+};
+
+export default App;
